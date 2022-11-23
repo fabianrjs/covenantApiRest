f3fad15f8e443719bc8306d03c6debac2790b426
William Rodriguez <wrodriguezb53@gmail.com>
2022-11-23 17:57:20 -0500

    votos agregados

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(express.json())
 
 
 
- app.get('/usuarios', async(req,res)=>{
+ app.get('/usuarios', async(req,res)=>{ //https://proyecto-calidad-back.herokuapp.com/usuarios
     const users = await pool.query('SELECT * FROM usuario')
     res.send({
         message: users.rows
     })
 })
 
- app.post('/usuarios/add', async(req,res) =>{
+ app.post('/usuarios/add', async(req,res) =>{ //https://proyecto-calidad-back.herokuapp.com/usuarios/add
     const {idusuario, nombre, correo, tipo, experto} = req.body;
     const sentencia = "INSERT INTO usuario(idusuario,nombre,correo,tipo,experto) VALUES('"+idusuario+"','"+nombre+"', '"+correo+"', '"+tipo+"', '"+experto+"');"
     if(idusuario && nombre && correo && tipo && experto){
@@ -29,14 +29,14 @@ app.use(express.json())
     
  })   
 
-app.get('/mecanismos', async(req,res)=>{
+app.get('/mecanismos', async(req,res)=>{ //https://proyecto-calidad-back.herokuapp.com/mecanismos
     const mecanismos = await pool.query('SELECT * FROM mecanismo')
     res.send({
         message: mecanismos.rows
     })
 })
 
- app.post('/mecanismo/add', async(req,res) =>{
+ app.post('/mecanismo/add', async(req,res) =>{ //https://proyecto-calidad-back.herokuapp.com/mecanismos/add
     const {idmecanismo, estado} = req.body;
     const sentencia = "INSERT INTO mecanismo(idmecanismo,estado) VALUES('"+idmecanismo+"', '"+estado+"');"
     if(idmecanismo && estado){
@@ -48,7 +48,7 @@ app.get('/mecanismos', async(req,res)=>{
 
  })
 
- app.get('/propuestas/:idmecanismo', async(req,res)=>{
+ app.get('/propuestas/:idmecanismo', async(req,res)=>{ //https://proyecto-calidad-back.herokuapp.com/propuestas/{idmecanismo}
     let idmecanismopar = req.params.idmecanismo;
     const sentencia = "SELECT * FROM propuesta WHERE idmecanismo="+idmecanismopar+";"
     const propuestas = await pool.query(sentencia)
@@ -63,7 +63,7 @@ app.get('/mecanismos', async(req,res)=>{
 
  })
 
- app.post('/propuesta/add', async(req,res) =>{
+ app.post('/propuesta/add', async(req,res) =>{ //https://proyecto-calidad-back.herokuapp.com/propuesta/add
     const{idpropuesta, votos, idmecanismo, autor} = req.body;
     const sentencia = "INSERT INTO propuesta(idpropuesta,votos,idmecanismo,autor) VALUES('"+idpropuesta+"', '"+votos+"', '"+idmecanismo+"', '"+autor+"');"
     const result = await pool.query(sentencia)
@@ -71,7 +71,16 @@ app.get('/mecanismos', async(req,res)=>{
 
  })
 
+ app.put('/votar/:propuestaid/:mecanismoid', async(req,res) =>{
+    let propid = req.params.propuestaid;
+    let mecid = req.params.mecanismoid;
+    const votosbuscar = "SELECT SUM(votos) FROM propuesta WHERE idmecanismo="+mecid+" AND idpropuesta="+propid+";"
+    const votar = await pool.query(votosbuscar)
+    const sentencia = "UPDATE propuesta SET votos="+4+" WHERE idmecanismo="+mecid+" AND idpropuesta="+propid+";"
+    const result = await pool.query(sentencia)
+    res.send('Voto agregado')
 
+ })
 
 
 app.listen(process.env.PORT || 3000)
