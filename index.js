8f177268bd8583fc7771cc37f150c86d7e5e4d83
William Rodriguez <wrodriguezb53@gmail.com>
2022-11-23 00:39:36 -0500

    new mecanismos/propuestas endpoints added

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ app.use(express.json())
 
 
  app.get('/usuarios', async(req,res)=>{
-    const users = await pool.query('SELECT * FROM usuarios')
+    const users = await pool.query('SELECT * FROM usuario')
     res.send({
         message: users.rows
     })
 })
 
  app.post('/usuarios/add', async(req,res) =>{
-    const {nombre, correo, tipo, experto} = req.body;
-    const sentencia = "INSERT INTO usuarios(nombre,correo,tipo,experto) VALUES('"+nombre+"', '"+correo+"', '"+tipo+"', '"+experto+"');"
-    if(nombre && correo && tipo && experto){
+    const {idusuario, nombre, correo, tipo, experto} = req.body;
+    const sentencia = "INSERT INTO usuario(idusuario,nombre,correo,tipo,experto) VALUES('"+idusuario+"','"+nombre+"', '"+correo+"', '"+tipo+"', '"+experto+"');"
+    if(idusuario && nombre && correo && tipo && experto){
         const result = await pool.query(sentencia) 
         res.send('Usuario creado')      
     }else{
@@ -29,7 +29,50 @@ app.use(express.json())
     
  })   
 
- 
+app.get('/mecanismos', async(req,res)=>{
+    const mecanismos = await pool.query('SELECT * FROM mecanismo')
+    res.send({
+        message: mecanismos.rows
+    })
+})
+
+ app.post('/mecanismo/add', async(req,res) =>{
+    const {idmecanismo, estado} = req.body;
+    const sentencia = "INSERT INTO mecanismo(idmecanismo,estado) VALUES('"+idmecanismo+"', '"+estado+"');"
+    if(idmecanismo && estado){
+        const result = await pool.query(sentencia) 
+        res.send('Mecanismo creado')      
+    }else{
+        res.send('Datos invalidos')
+    }
+
+ })
+
+ app.get('/propuestas/:idmecanismo', async(req,res)=>{
+    let idmecanismopar = req.params.idmecanismo;
+    const sentencia = "SELECT * FROM propuesta WHERE idmecanismo="+idmecanismopar+";"
+    const propuestas = await pool.query(sentencia)
+    if(propuestas){
+        res.send({
+            message: propuestas.rows
+        })
+    }else{
+        res.send('No tiene propuestas')
+    }
+    
+
+ })
+
+ app.post('/propuesta/add', async(req,res) =>{
+    const{idpropuesta, votos, idmecanismo, autor} = req.body;
+    const sentencia = "INSERT INTO propuesta(idpropuesta,votos,idmecanismo,autor) VALUES('"+idpropuesta+"', '"+votos+"', '"+idmecanismo+"', '"+autor+"');"
+    const result = await pool.query(sentencia)
+    res.send('propuesta agregada')
+
+ })
+
+
+
 
 app.listen(process.env.PORT || 3000)
 console.log('Server on port ', process.env.PORT || 3000)
